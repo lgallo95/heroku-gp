d506cf76fea93a2f8ab0be031f47486214193fe9
lgallo95 <lgallo2005@gmail.com>
2022-01-13 10:15:08 -0800

    wip

---
+++ index.js
@@ -0,0 +1,30 @@
+// const cohort1 = process.argv[2]
+// const user = process.env.USER
+// const shell = process.env.SHELL
+
+
+// if (cohort1 === 'web-49') {
+//     console.log(`${cohort1} is the best`)
+// }
+// else {
+//     console.log(`boooh cohort ${cohort1}`)
+// }
+
+// console.log(`user is ${user}`)
+// console.log(`shell is ${shell}`)
+
+
+ require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req,res) => {
+    res.json({ message: 'hey there'})
+})
+
+const port = process.env.PORT || 9000
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
